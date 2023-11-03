469941a769679c1d3116e17b5d5850beedeb34ac
AntuzAdolfo <antumaxis@gmail.com>
2023-11-03 16:22:37 -0300

    Agregando redireccionador de rutas inexistentes en app

--- app.js
+++ app.js
@@ -18,7 +18,18 @@ app.get('/home', (req, res) => {
     res.sendFile(homePath);
 });
 
+/*Colocarlo al final para que no genere conflictos con las demás rutas*/
+app.get('*', (req, res) => {
+    res.send(`
+    <div>
+       <h1>Esta página no existe</h1>
+       <a href="/home">Volver al Home</a>    
+    </div>
+    `)
+})
+
 app.listen(port, () => {
-    console.log(`Servidor levantado correctamente en el puerto ${port}`);
+    console.log(`Servidor levantado correctamente en el puerto ${port} 
+    Ir a la página: http://localhost:3000/`);
 });
 
